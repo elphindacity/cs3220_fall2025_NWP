dd34e2b88cdc12218afcf79f9a0611f5ac96b88e
ElphinDacity <66043411+elphindacity@users.noreply.github.com>
2025-10-11 13:46:20 -0600

    Add files via upload

--- environmentClass.py
+++ environmentClass.py
@@ -57,7 +57,7 @@ class Environment:
 
   def add_thing(self, thing, location=None):
     #from agentClass import Agent
-    from src.problemSolvingAgentProgramClass import SimpleProblemSolvingAgentProgram
+    from problemSolvingAgentProgramClass import SimpleProblemSolvingAgentProgram
     if thing in self.agents:
       print("Can't add the same agent twice")
     else:
